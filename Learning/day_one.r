c15ac98b659647d7f144faf761d2c54e7860a6c4
Muhammad Zafeer <zafeerwains@gmail.com>
2024-04-28 18:02:09 +0500

    day one

---
+++ Learning/day_one.r
@@ -0,0 +1,39 @@
+print("hello world")
+print(max(10, 3, 4))
+test <- abs(-4.7)
+print(test)
+test <- ceiling(-4.7)
+print(test)
+test <- floor(-4.7)
+print(test)
+test <- sqrt(17)
+print(test)
+test <- "hello world,12"
+print(paste(test, test))
+# test
+test <- "Testing cat"
+cat(test) # to explore more
+print(test)
+test <- "Testing cat"
+test <- nchar(test)
+print(test)
+test <- "Testing cat"
+test <- grepl("z", test)
+print(test)
+test <- "testing cat \"good\" to"
+print(test)
+cat(test)
+nested_function <- function(x, y) {
+    a <- x + y
+    return(a)
+}
+
+print(Nested_function(Nested_function(2, 2), Nested_function(3, 3)))
+
+numbers <- c(1, 2, 3)
+
+# Print numbers
+print(numbers)
+
+
+# Toady did from Start to R function From W3School
